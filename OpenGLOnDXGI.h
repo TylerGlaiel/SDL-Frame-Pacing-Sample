dc7a3d291ef9957c1394c5e33468e5b2915cd918
Tyler Glaiel <TylerGlaiel@users.noreply.github.com>
2024-07-07 16:49:37 -0700

    Delete OpenGLOnDXGI.h

--- OpenGLOnDXGI.h
+++
@@ -1,9 +0,0 @@
-#include <SDL3/SDL.h>
-
-struct OpenGLContext;
-
-OpenGLContext* CreateDXGIGLContext(SDL_Window* window);
-void DXGIGLResize(OpenGLContext* context, int width, int height);
-void DXGIGLPrepareBuffers(OpenGLContext* context);
-void DXGIGLSwapBuffers(OpenGLContext* context, bool vsync);
-int64_t DXGIGLGetPreviousSwapTimestamp(OpenGLContext* context);
